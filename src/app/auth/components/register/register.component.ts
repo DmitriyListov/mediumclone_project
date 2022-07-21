946657d6151d9014063b9015f3608966114f8f23
DmitriyListov <dmytro.lystov@loopme.com>
2022-07-21 21:18:00 +0300

    added register form

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,10 +1,27 @@
-import {Component} from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector:'mc-register',
-  templateUrl:'./register.component.html',
-  styleUrls:['./register.component.scss']
+  selector: 'mc-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent{
+export class RegisterComponent implements OnInit {
+  public registerForm: FormGroup;
+  constructor(private _formBuilder: FormBuilder) {}
 
+  public ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  public initializeForm(): void {
+    this.registerForm = this._formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+  public onSubmit(): void {
+    console.log(this.registerForm.valid);
+  }
 }
