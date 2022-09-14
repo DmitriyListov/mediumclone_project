92e462f813d4b08e8b935697e31d3b0f374ac2e2
DmitriyListov <dmytro.lystov@loopme.com>
2022-09-14 12:32:57 +0300

    updated register reducer

--- src/app/auth/types/authState.interface.ts
+++ src/app/auth/types/authState.interface.ts
@@ -1,3 +1,9 @@
+import { ICurrentUser } from '../../shared/types/curentUser.interface';
+import { IBackendErrors } from '../../shared/types/backendErrors.interface';
+
 export interface IAuthState {
   isSubmitting: boolean;
+  currentUser: ICurrentUser | null;
+  isLoggedIn: boolean | null;
+  validationErrors: IBackendErrors | null;
 }
